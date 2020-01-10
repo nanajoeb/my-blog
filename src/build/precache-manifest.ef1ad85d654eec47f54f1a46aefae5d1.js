0b9e20121e83e7da38671ab09e94398429b7919a
nanajoeb <achin.eng@live.com>
2020-01-10 20:29:55 +0000

    first commit

---
+++ src/build/precache-manifest.ef1ad85d654eec47f54f1a46aefae5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9858d382506a89e569baad1a499717ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "998eda14646d5ab8d2e2",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "ecf191b3305c2ded2ca3",
+    "url": "/static/js/2.7cfe22a5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7cfe22a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "998eda14646d5ab8d2e2",
+    "url": "/static/js/main.c5b80fa1.chunk.js"
+  },
+  {
+    "revision": "483de12b1103ab97ea36",
+    "url": "/static/js/runtime-main.71adc4b6.js"
+  }
+]);
